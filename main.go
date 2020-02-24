36d59b83a17fcb44f5dd2587d2b659125f699553
Ahmed-gubara <ahmed.gubara93@gmail.com>
2020-02-25 01:10:15 +0200

    create server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "net"
 
+import "fmt"
+
+import "google.golang.org/grpc"
+
+type server struct {
+}
+
+const port int = 6000
+
+func main() {
+	lis, _ := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	grpcServer := grpc.NewServer()
+	grpcServer.Serve(lis)
 }
